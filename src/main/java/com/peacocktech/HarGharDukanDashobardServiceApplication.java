b78cad37314d098b94a5ff28a3c822c84c6a071e
kanks <kanks@Kankshi_sharma>
2023-04-09 00:38:07 +0530

    first commit

---
+++ src/main/java/com/peacocktech/HarGharDukanDashobardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.peacocktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarGharDukanDashobardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarGharDukanDashobardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peacocktech/HarGharDukanDashobardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peacocktech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarGharDukanDashobardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
